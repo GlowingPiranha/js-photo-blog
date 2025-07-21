2f1284b7c53c3373170fe3d5666b101c6be8025c
GlowingPiranha <ikimus01@gmail.com>
2025-07-21 17:40:19 +0200

    [upd] console.log-data/first-try-js

---
+++ script.js
@@ -0,0 +1,23 @@
+const generatePolaroid = (item) => {
+  const { url, title, date } = item;
+
+  const polaroidCard = `<div class="col d-flex justify-content-center align-items-center">
+          <div class="polaroid-wrapper">
+            <img src="/assets/img/pin.svg" alt="pin" class="pin-img">
+            <div class="polaroid">
+              <img src="${url}" alt="dummy" class="img-width">
+              <div class="text font-edu">${title}</div>
+              <div class="text font-mono">${date}</div>
+            </div>
+          </div>
+        </div>`;
+
+  return polaroidCard;
+};
+
+
+const container = document.getElementById(`multi-polaroid`);
+
+axios.get('https://lanciweb.github.io/demo/api/pictures/').then((resp) => {
+  console.log(resp.data)
+});
